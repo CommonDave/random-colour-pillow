7694c5af64f76ea777ccd717b32cad0cc61c77b6
David Drobner <ddrobner03@gmail.com>
2017-12-15 12:43:05 -0500

    aaa

--- main.py
+++ main.py
@@ -8,4 +8,7 @@ blue = random.randint(1, 255)
 green = random.randint(1, 255)
 name = random.randint(1, 999999999999)
 
+size = str(width) + ", " + str(height)
+colour = str(red) + ", " + str(green) + ", " + str(blue)
+
 img = Image.new('RGB', (width, height))
